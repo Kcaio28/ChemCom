d1fbf9a30b20978b4d9a6466f3a1c76bfb6a8d59
CAIO CARNEIRO FERRER MORAES <25170980@educ123.sp.senai.br>
2025-11-11 17:05:35 -0300

    Começo do cadastro e login de empresas

---
+++ routes/clienteRotas.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import UsuarioModel from '../models/UsuarioModel.js';
+
+const router = express.Router;
+
+router.post('/cadastro', async (req, res) =>{
+    try {
+        const {nome, cnpj, email, senha} =req.body;
+
+        const existe = await UsuarioModel.buscarPorEmail(email);
+        if (existe) {
+            return res.status(400).json({mensagem: 'Este email já está cadastrado'});
+        }
+
+        await UsuarioModel.criar({nome, cnpj, email, senha});
+        res.status(201).json({mensagem: 'Empresa cadastrada com sucesso!'});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({mensagem: 'Erro ao cadastrar empresa.'});
+    }
+});
+
+router.post('/login', async (req, res) =>{
+    try {
+        const {email, senha} = req.body;
+        const usuario = await UsuarioModel.verificarCredenciais(email, senha);
+
+        if (!usuario) {
+            return res.status(401).json({mensagem: 'Email ou senha inválidos.'});
+        }
+
+        res.json({mensagem: 'Login bem-sucedido!', usuario});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({mensagem: 'Erro ao realizar login.'});
+    }
+});
+
+export default router;
