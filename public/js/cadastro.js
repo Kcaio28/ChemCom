91bfe13c951f37d6168e0fe7e6c912dfad033e48
MuriloSilva7 <paulomerola26@gmail.com>
2025-12-05 14:07:02 -0300

    Ignora

--- public/js/cadastro.js
+++ public/js/cadastro.js
@@ -4,7 +4,7 @@ document.getElementById("formCadastro").addEventListener("submit", async (e) => 
     const formData = new FormData(e.target);
     const dados = Object.fromEntries(formData.entries());
 
-    const resposta = await fetch("http://localhost:3000/clienteRotas/cadastro", {
+    const resposta = await fetch("/clienteRotas/cadastro", {
         method: "POST",
         headers: { "Content-type": "application/json" },
         body: JSON.stringify(dados)
